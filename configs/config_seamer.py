17bbc23fe5ce1737435cab109841b57ba8fed4a5
Alina <10210554+alina-miron@users.noreply.github.com>
2021-05-02 20:04:50 +0100

    Some code cleanup

--- configs/config_seamer.py
+++ configs/config_seamer.py
@@ -1,7 +1,9 @@
 
 # the path to the dataset, RawData folder, eg: FinalSkeletonDatasetV2.0/RawData/
 path_dataset = ""
-# path where to save the stats
+# the path to the dataset, RawData folder, eg: FinalSkeletonDatasetV2.0/Simplified/
+path_dataset_simplified = ""
+# path where to save the stats/figure eg: FinalSkeletonDatasetV2.0/EDA/ ;
+# Create the folder if it doesn't exist
 path_output = ""
-simplified = False
 feature_dim = 75
